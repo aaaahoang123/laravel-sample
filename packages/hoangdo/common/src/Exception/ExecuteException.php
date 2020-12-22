a3768b8ac2e7196306c1bd372074e7d124323989
Hoang Do <shinkenger.vn9x@gmail.com>
2020-12-22 12:17:56 +0700

    Initial commit.

--- packages/hoangdo/common/src/Exception/ExecuteException.php
+++ packages/hoangdo/common/src/Exception/ExecuteException.php
@@ -6,20 +6,12 @@ use Exception;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Throwable;
 
-class ExecuteException extends Exception implements HttpExceptionInterface
+class ExecuteException extends Exception
 {
     public function __construct($message = "", $code = 404, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
 
-    public function getStatusCode()
-    {
-        // TODO: Implement getStatusCode() method.
-    }
 
-    public function getHeaders()
-    {
-        // TODO: Implement getHeaders() method.
-    }
 }
